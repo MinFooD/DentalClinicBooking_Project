76d2372b7605d58f6c288e668eb06ffcaec946c9
phucLong <phucvahau157359@gmail.com>
2024-06-26 03:32:41 +0700

    cập nhật html,css của service + cập nhật chức năng Nha sĩ (html,css,controller)

---
+++ DentalClinicBooking_Project/Controllers/DentistController.cs
@@ -0,0 +1,73 @@
+﻿
+using DentalClinicBooking_Project.Data;
+using DentalClinicBooking_Project.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DentalClinicBooking_Project.Controllers
+{
+    public class DentistController : Controller
+    {
+        public DentalClinicBookingProjectContext _context;
+        //public IActionResult ShowDentistInfo(int id)
+        //{
+        //    _context = new DentalClinicBookingProjectContext();
+
+        //    var dentist = _context.Dentists.Select(a => new DentistWithClinicName
+        //    {
+        //        DentistId = a.DentistId,
+        //        DentistName = a.DentistName,
+        //        Image = a.Image,
+        //        Experience = a.Experience,
+        //        BasicId = a.BasicId,
+        //        AccountId = a.AccountId,
+        //        ClinicName = a.Basic.Clinic.ClinicName,
+        //        Description = a.Description,
+        //        Address = a.Basic.Address
+        //    }).FirstOrDefault(x => x.DentistId == id);
+
+        //    if (dentist != null)
+        //    {
+        //        return View(dentist);
+        //    }
+        //    return View(null);
+        //}
+
+        [HttpGet]
+        public IActionResult ShowAllDentist(string searchString, int page = 1)
+        {
+            _context = new DentalClinicBookingProjectContext();
+
+            var dentists = _context.Dentists.Select(a => new DentistWithClinicName
+            {
+                DentistId = a.DentistId,
+                DentistName = a.DentistName,
+                Image = a.Image,
+                Experience = a.Experience,
+                BasicId = a.BasicId,
+                AccountId = a.AccountId,
+                ClinicName = a.Basic.Clinic.ClinicName,
+                Address = a.Basic.Address
+            }).Where(d => d.DentistName.Contains(searchString) || string.IsNullOrEmpty(searchString)).ToList();
+
+            var searchDentists = new SearchDentist
+            {
+                SearchString = searchString,
+                dentists = dentists,
+                ResultCount = dentists.Count()
+            };
+
+            //paging
+            int NoOfDentistPerPage = 4;
+            var NoOfPaging = Math.Ceiling((decimal)dentists.Count() / NoOfDentistPerPage);
+            int NoOfDentistToSkip = (page - 1) * NoOfDentistPerPage;
+            ViewBag.Page = page;
+            ViewBag.NoOfPaging = NoOfPaging;
+            searchDentists.dentists = searchDentists.dentists.Skip(NoOfDentistToSkip).Take(NoOfDentistPerPage).ToList();
+            //
+
+            return View(searchDentists);
+
+
+        }
+    }
+}
---
+++ DentalClinicBooking_Project/Models/ViewModels/SearchDentist.cs
@@ -0,0 +1,9 @@
+﻿namespace DentalClinicBooking_Project.Models.ViewModels
+{
+    public class SearchDentist()
+    {
+        public string SearchString { get; set; }
+        public List<DentistWithClinicName> dentists { get; set; }
+        public int ResultCount { get; set; }
+    }
+}
