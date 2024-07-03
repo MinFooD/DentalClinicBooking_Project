015700ec1dc9c605be84f121bb04989f2482be74
phucLong <phucvahau157359@gmail.com>
2024-07-03 17:36:23 +0700

    Cập nhật Login, Logout + mã hóa Password

---
+++ DentalClinicBooking_Project/Models/ViewModels/LoginVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DentalClinicBooking_Project.Models.ViewModels
+{
+	public class LoginVM
+	{
+		[Required(ErrorMessage ="Gmail can not be blank.")]
+		public string Gmail { get; set; }
+		[Required(ErrorMessage = "Password can not be blank.")]
+		public string Password { get; set; }
+	}
+}
