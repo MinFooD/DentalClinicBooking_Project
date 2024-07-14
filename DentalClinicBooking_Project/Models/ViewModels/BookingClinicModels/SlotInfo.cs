805208260a066f3ad9432b23ad15cc9bce4e59a9
NoOneTopVN <huynhtrongnghia15n@gmail.com>
2024-07-14 17:17:37 +0700

    Cập nhật BookClinic

---
+++ DentalClinicBooking_Project/Models/ViewModels/BookingClinicModels/SlotInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace DentalClinicBooking_Project.Models.ViewModels.BookingClinicModels
+{
+    public class SlotInfo
+    {
+        public int Count { get; set; }
+        public required string DateTime { get; set; } 
+    }
+}
